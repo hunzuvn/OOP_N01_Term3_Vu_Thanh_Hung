23540aa8e2b3a17b166e5324217ece67e6f1e93d
Vu Thanh Hung <hung.thanhvu24@gmail.com>
2025-05-30 08:31:16 +0000

    up

--- OOP/test/UserListTest.java
+++ OOP/test/UserListTest.java
@@ -1,23 +1,6 @@
 package com.example.librarymanagement.test;
 
 import com.example.librarymanagement.entity.UserEntity;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-public class UserEntityTest {
-
-    @Test
-    public void testUserEntitySettersAndGetters() {
-        UserEntity user = new UserEntity();
-        user.setId(1L);
-        user.setName("John Doe");
-        user.setEmail("john@example.com");
-        user.setPhone("123456789");
-
-        assertEquals(1L, upackage com.example.librarymanagement.test;
-
-import com.example.librarymanagement.entity.UserEntity;
 import com.example.librarymanagement.service.UserList;
 
 public class UserListTest {
@@ -44,10 +27,5 @@ public class UserListTest {
         userList.removeUserById(1L);
         System.out.println("Sau khi xoá:");
         System.out.println(userList.getAllUsers());
-    }
-}ser.getId());
-        assertEquals("John Doe", user.getName());
-        assertEquals("john@example.com", user.getEmail());
-        assertEquals("123456789", user.getPhone());
     }
 }
