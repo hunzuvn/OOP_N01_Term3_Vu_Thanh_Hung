42fd96081cb2139fdb265222f9534c576ba38400
Vu Thanh Hung <hung.thanhvu24@gmail.com>
2025-05-30 08:37:51 +0000

    -up

--- OOP/test/BookListTest.java
+++ OOP/test/BookListTest.java
@@ -1,24 +1,30 @@
 package com.example.librarymanagement.test;
 
 import com.example.librarymanagement.entity.BookEntity;
+import com.example.librarymanagement.entity.CategoryEntity;
 import com.example.librarymanagement.service.BookList;
+
+import java.time.LocalDate;
 
 public class BookListTest {
     public static void main(String[] args) {
         BookList bookList = new BookList();
 
+        // Tạo danh mục sách mẫu
+        CategoryEntity category = new CategoryEntity(1L, "Khoa học");
+
         // Tạo sách mẫu
-        BookEntity book = new BookEntity(1L, "Effective Java", "Joshua Bloch", "Programming", 5);
+        BookEntity book = new BookEntity(1L, "Vũ trụ kỳ bí", "Stephen Hawking", "1234567890", LocalDate.of(2020, 5, 20), 10, 10, category);
 
         // Thêm sách
         bookList.addBook(book);
 
-        // Hiển thị sách
+        // Hiển thị danh sách sách
         System.out.println("Danh sách sách sau khi thêm:");
         System.out.println(bookList.getAllBooks());
 
         // Cập nhật sách
-        BookEntity updatedBook = new BookEntity(1L, "Effective Java - Updated", "Joshua Bloch", "Programming", 10);
+        BookEntity updatedBook = new BookEntity(1L, "Vũ trụ kỳ bí - Bản cập nhật", "Stephen Hawking", "1234567890", LocalDate.of(2020, 5, 20), 15, 15, category);
         bookList.updateBook(1L, updatedBook);
         System.out.println("Sau khi cập nhật:");
         System.out.println(bookList.getBookById(1L));
